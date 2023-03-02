466b60706c5e14a091440cc33da4e5b6ce8cde35
Mr4NaniReddy <dumpabhargav22@gmail.com>
2023-03-02 14:32:25 +0530

    added some text

---
+++ p3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+
+int main(){
+    // int arr[5] = {4,4,5,3,1};
+    // int x, y;
+    // int total_sum = 0;
+    // int total_sqsum = 0;
+    // // sum of n natural numbers n*(n+1)/2
+    // // sum of squares of numbers n*(n+1)*(2n+1)/6
+    // int l = sizeof(arr)/sizeof(arr[0]);
+    // for(int i=0; i<l; i++){
+
+    // }
+
+
+ return 0;
+}
